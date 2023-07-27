86e047d91cec8c02480786d2eec7b32886947dfe
supriya701 <61152281+supriya701@users.noreply.github.com>
2023-07-27 16:01:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# javascript-star-pattern
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/javascript-pfekv7)
---
+++ index.js
@@ -0,0 +1,46 @@
+
+//*****
+// *****
+// *****
+// *****
+// *****
+
+// let string = '';
+// for (let i = 0; i < 5; i++) {
+//   for (let j = 0; j < 5; j++) {
+//     string += '*';
+//   }
+//   string += '\n';
+// }
+// console.log(string);
+
+// *****
+// ****
+// ***
+// **
+// *
+
+// let string = '';
+// for (let i = 0; i < 5; i++) {
+//   for (let j = i; j < 5; j++) {
+//     string += '*';
+//   }
+//   string += '\n';
+// }
+// console.log(string);
+
+// *
+// **
+// ***
+// ****
+// *****
+
+// let string = '';
+// for (let i = 0; i < 5; i++) {
+//   for (let j = 0; j <=i; j++) {
+//     string += '*';
+//   }
+//   string += '\n';
+// }
+// console.log(string);
+
